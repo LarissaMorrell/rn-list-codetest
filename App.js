2c748113bb765beb71002c089e5ce46c92358d19
Larissa Morrell <larissa@larissamorrell.com>
2023-05-19 09:59:13 -0400

    fix btn to enable according to input, feature add darkmode

--- App.js
+++ App.js
@@ -8,6 +8,9 @@
 import React, {useState} from 'react';
 import {
   SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Switch,
   TextInput,
   Text,
   TouchableOpacity,
@@ -16,36 +19,47 @@ import {
 
 function App() {
   const [input, setInput] = useState('');
-  const [list, setList] = useState([{ title: 'oneitem', id: 'todo-oneitem'}]);
+  const [list, setList] = useState([{title: 'oneitem', id: 'todo-oneitem'}]);
+  const [darkModeEnabled, toggleDarkMode] = useState(false);
 
-  const handleAddItem = () => { //Assuming duplicates are allowed and not adjusting for punctuation
-    console.log('handleAddItem');
-    const newList = {title: input, id: input.toLowerCase().replaceAll(' ', '-')};
-
+  const handleAddItem = () => {
+    //Assuming duplicates are allowed and not adjusting for punctuation
+    const newList = {
+      title: input,
+      id: input.toLowerCase().replaceAll(' ', '-'),
+    };
+    console.log(input, newList, [...list, newList]);
     setList([...list, newList]);
     setInput('');
-  }
+  };
 
-  const handleRemoveItem = (item) => {
+  const handleRemoveItem = item => {
     const removedIndex = list.indexOf(item);
     const newList = [...list];
     newList.splice(removedIndex, 1);
     setList(newList);
-  }
+  };
 
-  const ToDo = (item) => (
+  const ToDo = item => (
     <TouchableOpacity
       testID="todo-id"
       key={item.id}
-      onPress={() => handleRemoveItem(item)}
-    >
+      onPress={() => handleRemoveItem(item)}>
       <Text>{item.title}</Text>
     </TouchableOpacity>
   );
 
   return (
-    <SafeAreaView>
-      <View testID="wrapper-id">
+    <View
+      testID="wrapper-id"
+      style={{backgroundColor: darkModeEnabled ? '#888' : '#fff'}}>
+      <SafeAreaView>
+        <Switch
+          trackColor={{false: '#767577', true: '#81b0ff'}}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleDarkMode}
+          value={darkModeEnabled}
+        />
         <TextInput
           testID="input-id"
           value={input}
@@ -53,18 +67,28 @@ function App() {
           style={{borderWidth: 1}}
         />
         <TouchableOpacity
-          disabled={!!list.length}
+          disabled={input.length === 0}
           testID="add-button-id"
           onPress={handleAddItem}
-        >
+          style={styles.button}>
           <Text>Add</Text>
         </TouchableOpacity>
-        <View>
-          {list.map((item) => ToDo(item))}
-        </View>
-      </View>
-    </SafeAreaView>
+        <ScrollView>
+          {list.map(item => ToDo(item))}
+        </ScrollView>
+      </SafeAreaView>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    width: 100,
+    paddingVertical: 5,
+    backgroundColor: '#81b0ff',
+    alignItems: 'center',
+    alignSelf: 'center',
+  },
+});
 
 export default App;
